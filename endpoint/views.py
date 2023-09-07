f66791f510bcd64be4b33a318610d43168cc279f
MAC <horlarmihleykan10@gmail.com>
2023-09-07 15:50:46 +0100

    new

--- endpoint/views.py
+++ endpoint/views.py
@@ -10,12 +10,12 @@ def index(request):
     track = request.GET.get("track")
 
     # Get current UTC time
-    utc_time = datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    utc = datetime.now(pytz.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     response = {
         "slack_name": slack_name,
         "current_day": datetime.now().strftime("%A"),
-        "utc_time": utc_time,
+        "utc_time": utc,
         "track": track,
         "github_file_url": "https://github.com/Shabzynana/Stage_One/blob/main/endpoint/views.py",
         "github_repo_url": "https://github.com/Shabzynana/Stage_One",
